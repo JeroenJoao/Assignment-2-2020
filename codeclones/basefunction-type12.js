5339a5c03c934cebd19f13467894757b6b5c0472
Daniël Verloop <a.c.verloop@student.tue.nl>
2021-03-06 10:36:29 +0100

    created all type-1 clones

---
+++ codeclones/basefunction-type12.js
@@ -0,0 +1,12 @@
+function findNumber(a, key) {
+    if (a.length == 0) { return -1; }
+
+    var index = 0;
+    while(index < a.length) {
+        if (a[index] == key) { return key;}
+        index++;
+    }
+    return -1;
+}
+
+find([1,2,3,4,5,6], 6);
